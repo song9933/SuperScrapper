0248a83a39b90cf7c89c96aa4e3aa41f4c09f3c0
SONG JOOYOUNG <sngbrm@gmail.com>
2020-07-06 09:25:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+
+app = Flask("SuperScrapper")
+
+@app.route("/")
+def home():
+  return render_template("home.html")
+
+@app.route("/report")
+def report():
+  word = request.args.get('word')
+  return render_template("report.html", searchingBy=word)
+
+app.run(host="0.0.0.0")
