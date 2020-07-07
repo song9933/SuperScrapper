acd6671308dbc3e54040c02d76b6e2da85934468
SONG JOOYOUNG <sngbrm@gmail.com>
2020-07-07 02:49:02 +0000

    rendering jobs

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+from scrapper import get_jobs
 
 app = Flask("SuperScrapper")
+db = {}
+
 
 @app.route("/")
 def home():
-  return render_template("home.html")
+    return render_template("home.html")
+
 
 @app.route("/report")
 def report():
-  word = request.args.get('word')
-  return render_template("report.html", searchingBy=word)
+    word = request.args.get('word')
+    if word:
+        word = word.lower()
+        existingJobs = db.get(word)
+        if existingJobs:
+            jobs = existingJobs
+        else:
+            jobs = get_jobs(word)
+            db[word] = jobs
+    else:
+        return redirect("/")
+    return render_template(
+        "report.html", 
+        searchingBy=word, 
+        resultsNumber=len(jobs),
+        jobs = jobs
+        )
+
 
 app.run(host="0.0.0.0")
